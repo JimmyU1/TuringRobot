e37384b8373984d62923f9c5f89a9c3373cf4b65
JimmyU1 <ljn19940214@hotmail.com>
2017-06-04 13:32:03 +0800

    create project

---
+++ js/action.js
@@ -0,0 +1,61 @@
+/**
+ * Created by Jimmy on 2017/6/2.
+ */
+var content = "You can talk to me.\n\n";
+var user = "JimmyU1";
+var robot = "小螃蟹";
+var msg = "11";
+$(function () {
+    $(document).keydown(function(event){
+        if(event.keyCode==13){
+            $("#btn").click();
+        }
+    });
+
+    $("#btn").bind('click',function () {
+        var message = "";
+        message = $("#msg").val();
+        addInfo();
+        sendMessage(message);
+        $("#msg").val("");
+    });
+
+    function addInfo(){
+        msg = $("#msg").val();
+        var info = "<p class='infotitle'>" + user + "\t"+ new Date() +  "</p>" + "<p class='info'>" + msg + "</p> ";
+        $("#content").append(info);
+
+    }
+
+    function addText(text) {
+        var content = "<p class='texttitle'>" + robot + "\t"+ new Date() + "</p>" + "<p class='text'>" + text + "</p> ";
+        $("#content").append(content);
+    }
+
+    function parseData(data) {
+        return data.text;
+    }
+
+    function sendMessage(message) {
+        console.log("ajax info: " + message);
+        $.ajax({
+            url: "http://www.tuling123.com/openapi/api?key=bbde1f790ee24aed8875a0b83b527668",
+            type: "get",
+            data: {info:message},
+            dataType:"json",
+            async: false,
+            success: function (data) {
+                var text = data.text;
+                addText(text);
+            },
+            error: function () {
+                $.messager.alert("Error",'There is a problem about sending message.');
+            }
+        });
+    }
+})
+
+
+
+
+
